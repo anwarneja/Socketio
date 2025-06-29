b4a24f2e1d394cf20528e378352feddb69bd52ff
anwarneja <anwarneja55@gmail.com>
2025-06-29 22:10:11 +0300

    names

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const io=new Server(server);
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/socket.html');
+    res.sendFile(__dirname + '/index.html');
   });
 
 server.listen(3000,()=>{
